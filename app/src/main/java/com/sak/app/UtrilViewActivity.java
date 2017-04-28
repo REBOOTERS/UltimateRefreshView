d62dc6854f644b0d15c09c541c2d84b078d03fcd
zhuyongqing <zhuyongqing@co-mall.com>
2017-04-28 11:31:03 +0800

    init commit

---
+++ app/src/main/java/com/sak/app/UtrilViewActivity.java
@@ -0,0 +1,116 @@
+package com.sak.app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.sak.app.subfragment.ListViewFragment;
+import com.sak.app.subfragment.RecyclerViewFragment;
+import com.sak.app.subfragment.ScrollViewFragment;
+import com.sak.app.subfragment.WebViewFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UtrilViewActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private List<Fragment> mFragments = new ArrayList<>();
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utril_view);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mFragments.add(new ListViewFragment());
+        mFragments.add(new RecyclerViewFragment());
+        mFragments.add(new ScrollViewFragment());
+        mFragments.add(new WebViewFragment());
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), mFragments);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> mFragmentList;
+
+        public SectionsPagerAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+            super(fm);
+            mFragmentList = fragmentList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return mFragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "ListView";
+                case 1:
+                    return "RecyclerView";
+                case 2:
+                    return "ScrollView";
+                case 3:
+                    return "WebView";
+            }
+            return null;
+        }
+    }
+}
